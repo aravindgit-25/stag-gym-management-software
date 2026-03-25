33bdae0123b20e1fa89957a2bd4e89f775d1e242
aravindgit-25 <233910863+aravindgit-25@users.noreply.github.com>
2026-03-25 14:51:25 +0530

    subscription

---
+++ backend/src/main/java/com/stag/gym/repository/SubscriptionRepository.java
@@ -0,0 +1,12 @@
+package com.stag.gym.repository;
+
+import com.stag.gym.model.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+    List<Subscription> findByMemberId(Long memberId);
+}
