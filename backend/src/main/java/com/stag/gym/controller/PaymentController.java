6bf345fb7b00747a9bdd2e83a414c763da72d4e5
aravindgit-25 <233910863+aravindgit-25@users.noreply.github.com>
2026-03-25 15:03:05 +0530

    payment - table

---
+++ backend/src/main/java/com/stag/gym/controller/PaymentController.java
@@ -0,0 +1,30 @@
+package com.stag.gym.controller;
+
+import com.stag.gym.dto.PaymentRequestDTO;
+import com.stag.gym.dto.PaymentResponseDTO;
+import com.stag.gym.service.PaymentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/payments")
+@RequiredArgsConstructor
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<PaymentResponseDTO> processPayment(@Valid @RequestBody PaymentRequestDTO requestDTO) {
+        return new ResponseEntity<>(paymentService.processPayment(requestDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PaymentResponseDTO>> getAllPayments() {
+        return ResponseEntity.ok(paymentService.getAllPayments());
+    }
+}
