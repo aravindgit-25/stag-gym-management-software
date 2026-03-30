9c6c077b9d9a08b1ab830a8d16f0a3d562503220
aravindgit-25 <233910863+aravindgit-25@users.noreply.github.com>
2026-03-30 12:57:44 +0530

    changes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://your-server-ip-or-domain:8080/api/v1' // We will update this later
+  apiUrl: 'https://gym-backend-o9ug.onrender.com/api/v1'
 };
